346b174c06793347ecec22b7618218440dcc457d
mallory-scotton <mallory.scotton@epitech.eu>
2025-02-12 17:08:33 +0100

    add: adding build scripts

---
+++ scripts/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Color constants
+CO_COLOR="\033[0;34m"
+OB_COLOR="\033[0;36m"
+OK_COLOR="\033[0;32m"
+ER_COLOR="\033[0;31m"
+WA_COLOR="\033[0;33m"
+NO_COLOR="\033[m"
+
+# Text constant
+compiling=`echo "compiling"`
+
+# Print fthe current object
+printf "%b%-46b" "$CO_COLOR$compiling " "$OB_COLOR$2$NO_COLOR"
+
+# Execute the command and parse the log
+$1 2> "$2.log";
+RESULT=$?
+
+# Check the result of the command
+if [[ $RESULT -ne 0 ]]; then
+  printf "%b%b\n" "$ER_COLOR" "[✖]$NO_COLOR          "
+  rm -rf .build
+  echo
+elif [ -s "$2.log" ]; then
+  printf "%b%b\n" "$WA_COLOR" "[⚠]$NO_COLOR          "
+else
+  printf "%b%b\n" "$OK_COLOR" "[✓]$NO_COLOR          "
+fi
+
+# Print the possible logs
+cat "$2.log"
+rm -f "$2.log"
+
+# Exit with the same status has the command
+exit $RESULT
