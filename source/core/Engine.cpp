45d967a80c4a33b037478e1794257c37d66db77d
mallory-scotton <mallory.scotton@epitech.eu>
2025-02-12 16:17:18 +0100

    add: adding toggle for debug mode and FPS display

--- source/core/Engine.cpp
+++ source/core/Engine.cpp
@@ -29,6 +29,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include "core/Engine.hpp"
 #include "states/MenuState.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui-SFML.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // Namespace tkd
@@ -46,16 +48,57 @@ Engine::Engine(bool debug)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+void Engine::renderFpsDisplay(void) const
+{
+    static const ImGuiWindowFlags window_flags =
+        ImGuiWindowFlags_NoDecoration |
+        ImGuiWindowFlags_AlwaysAutoResize |
+        ImGuiWindowFlags_NoSavedSettings |
+        ImGuiWindowFlags_NoFocusOnAppearing |
+        ImGuiWindowFlags_NoNav;
+    bool opened = true;
+
+    if (m_debug) {
+        auto& io = ImGui::GetIO();
+
+        ImGui::SetNextWindowBgAlpha(0.35f);
+        ImGui::Begin("FPS", &opened, window_flags);
+        ImGui::Text(
+            "FPS: %.2f (%.2gms)",
+            io.Framerate,
+            io.Framerate ? 1000.0f / io.Framerate : 0.0f
+        );
+        ImGui::End();
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
 void Engine::start(void)
 {
     sf::Event event;
     tkd::Packet packet;
-    // sf::Clock clock;
+    sf::Clock clock;
+    sf::Time restart;
+
+    if (!ImGui::SFML::Init(m_window))
+        exit(84);
+    ImGui::SFML::Update(m_window, sf::Time());
 
     while (m_window.isOpen()) {
+        restart = clock.restart();
+
+        if (m_debug)
+            ImGui::SFML::Update(m_window, restart);
+
         while (m_window.pollEvent(event)) {
+            if (m_debug)
+                ImGui::SFML::ProcessEvent(m_window, event);
             if (event.type == sf::Event::Closed)
                 m_window.close();
+            if (
+                event.type == sf::Event::KeyPressed &&
+                event.key.code == sf::Keyboard::Num0
+            ) m_debug = !m_debug;
             m_manager.handleEvent(event);
         }
 
@@ -64,13 +107,19 @@ void Engine::start(void)
 
         m_manager.update();
 
+        renderFpsDisplay();
+
         m_window.clear();
         m_manager.render();
+        if (m_debug)
+            ImGui::SFML::Render(m_window);
         m_window.display();
 
         if (m_manager.empty())
             m_window.close();
     }
+
+    ImGui::SFML::Shutdown(m_window);
 }
 
 } // namespace tkd
